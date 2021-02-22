96e65d8835f79c84e04082b26227be8b99197cf8
tanvirgeekios <tanvir.alam@iosventure.com>
2021-02-22 10:19:54 +0600

    Horizontal CollectionView Done

---
+++ CollectionVIewUIKit/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionVIewUIKit
+//
+//  Created by MD Tanvir Alam on 22/2/21.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var collectionVIewLabel: UILabel!
+}
--- CollectionVIewUIKit/ViewController.swift
+++ CollectionVIewUIKit/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    var movieNames = ["Fault in Our Stars", "500 days with summer", "About time","Lady Bird"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myCollectionView.delegate = self
+        myCollectionView.dataSource = self
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movieNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = myCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
+        cell.collectionVIewLabel.text = movieNames[indexPath.row]
+        cell.backgroundColor = .green
+        return cell
+    }
 
 }
 
